9258e1e57c7627f35b577340dded35ac8744586c
joeboadidev <joeboadi1@gmail.com>
2023-04-11 02:14:47 +0000

    working with functions

---
+++ functions.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int item(){
+
+    string arr [] = {"foods", "dresses", "devices"};
+
+    cout << arr[0] << endl;
+
+    return 0;
+}
+
+int main(){
+
+    item();
+    return 0;
+}
