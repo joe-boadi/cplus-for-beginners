9cd88d7a49c0ce0e228cc625997d6113056fa0af
joeboadidev <joeboadi1@gmail.com>
2023-04-12 08:23:58 +0000

    Understand Operartors: For study purpose only(No Code)

---
+++ operators.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   /*
+        A == B: A Equal to B
+        A != B: A is not Equal to B
+        A > B: A is greater than B
+        A < B: A is less than B
+        A <= B: A is less/equal to B
+        A >= B: A is greater/equal to B
+        int A, B = 10; 25;
+   */ 
+    
+    /*
+
+            LOGICAL OPERATOR.
+        A == B && A < 5: returns true if the two conditions are met.
+        A < B || A < C: returns true if only condition are met.
+        !(A<B): returns false if the result is true and true if result is false.
+
+    */
+
+    return 0;
+}
