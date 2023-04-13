739d3c5f87de3709020dabcb308331332b57702e
joeboadidev <joeboadi1@gmail.com>
2023-04-13 01:18:24 +0000

    applications

--- functions.cpp
+++ functions.cpp
@@ -2,17 +2,18 @@
 #include <cmath>
 using namespace std;
 
-int item(){
+int item( int value1, int value2 ){
 
     string arr [] = {"foods", "dresses", "devices"};
 
     cout << arr[0] << endl;
+    cout << value1 + value2 << endl;
 
     return 0;
 }
 
 int main(){
 
-    item();
+    item(4, 4);
     return 0;
 }
